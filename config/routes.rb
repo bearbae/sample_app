4e233ed27c0e8e188bfb38082bf4edfa9f6e9ea8
Doan Quoc Tien <doanquoctien3005@gmail.com>
2024-07-07 17:41:30 +0700

    chapter_8

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :products
   get 'demo_partials/new'
   get 'demo_partials/edit'
   get 'static_pages/home'
   get 'static_pages/help'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
